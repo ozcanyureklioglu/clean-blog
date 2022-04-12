7d4d47014c9d3bfe33fa66ca03e1f3031b1384f9
Özcan Yüreklioğlu <ozcan.yu@hotmail.com>
2022-04-12 22:49:20 +0300

    Odev 10 yapıldı

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ app.get('/post', (req, res) => {
   
   res.render('post');
 });
+app.get('/posts/:id', async (req, res) => {
+  const post=await Post.findById(req.params.id);
+  res.render('post',{
+    post
+  });
+
+});
 app.get('/add_post', (req, res) => {
   
   res.render('add_post');
